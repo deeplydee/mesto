e17a033da8f61d35577d2a1aba916753fb6f06d8
deeplydee <dmitry.pletyukhin@gmail.com>
2022-04-04 00:57:46 +0300

    refactor and style popup block

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form'); // Воспользуйтесь методом querySelector()
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__name-input'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.popup__job-input'); // Воспользуйтесь инструментом .querySelector()
+let formElement = document.querySelector('.popup__form'); // Находим форму в DOM
+let nameInput = document.querySelector('.popup__text-input_edit_name-profile'); // Находим поля формы в DOM
+let jobInput = document.querySelector('.popup__text-input_edit_job-profile');
 
 let popup = document.querySelector('.popup');
 let buttonOpenPopup = document.querySelector('.profile__edit-button');
@@ -11,12 +9,10 @@ let buttonClosePopup = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
+function formSubmitHandler (evt) { // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
 
     // Получите значение полей jobInput и nameInput из свойства value
     nameInputValue = (nameInput.value);
@@ -41,9 +37,7 @@ function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', formSubmitHandler); // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 
 buttonOpenPopup.addEventListener('click', openPopup);
 buttonClosePopup.addEventListener('click', closePopup);
