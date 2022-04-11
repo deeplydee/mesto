deafe6b4daf0b444f7c11b3cc0296e2fec6ca962
deeplydee <dmitry.pletyukhin@gmail.com>
2022-04-11 17:11:43 +0300

    form adding card

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,25 @@
-let formElement = document.querySelector('.popup__form'); // Находим форму в DOM
+let formElementEditProfile = document.querySelector('.popup__form'); // Находим форму в DOM
 let nameInput = document.querySelector('.popup__text-input_edit_name-profile'); // Находим поля формы в DOM
 let jobInput = document.querySelector('.popup__text-input_edit_job-profile');
 
-let popup = document.querySelector('.popup');
-let buttonOpenPopup = document.querySelector('.profile__edit-button');
-let buttonClosePopup = document.querySelector('.popup__close-button');
+let popupEditProfile = document.querySelector('.popup');
+let buttonOpenPopupProfile = document.querySelector('.profile__edit-button');
+let buttonClosePopupProfile = document.querySelector('.popup__close-button');
 
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
+
+//
+
+let formElementAddCard = document.querySelector('.popup__form_element_add-card'); // Находим форму в DOM
+let nameAddCardInput = document.querySelector('.popup__text-input_element_name-add-card'); // Находим поля формы в DOM
+let linkAddCardInput = document.querySelector('.popup__text-input_element_link-add-card');
+
+let popupAddCard = document.querySelector('.popup_add-card');
+let buttonOpenPopupAddCard = document.querySelector('.profile__add-button');
+let buttonClosePopupAddCard = document.querySelector('.popup__close-button_add-card');
+
+//
 
 function formSubmitHandler (evt) { // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
@@ -30,14 +42,41 @@ function formSubmitHandler (evt) { // Обработчик «отправки» 
 function openPopup() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-  popup.classList.add('popup_opened');
+  popupEditProfile.classList.add('popup_opened');
 }
 
 function closePopup() {
-  popup.classList.remove('popup_opened');
+  popupEditProfile.classList.remove('popup_opened');
 }
 
-formElement.addEventListener('submit', formSubmitHandler); // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+//
 
-buttonOpenPopup.addEventListener('click', openPopup);
-buttonClosePopup.addEventListener('click', closePopup);
+function formSubmitHandlerAddCard (evt) { // Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+
+  closePopupAddCard()
+}
+
+function openPopupAddCard() {
+  popupAddCard.classList.add('popup_opened');
+}
+
+function closePopupAddCard() {
+  popupAddCard.classList.remove('popup_opened');
+}
+
+//
+
+formElementEditProfile.addEventListener('submit', formSubmitHandler); // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+
+buttonOpenPopupProfile.addEventListener('click', openPopup);
+buttonClosePopupProfile.addEventListener('click', closePopup);
+
+//
+
+formElementAddCard.addEventListener('submit', formSubmitHandlerAddCard); // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+
+buttonOpenPopupAddCard.addEventListener('click', openPopupAddCard);
+buttonClosePopupAddCard.addEventListener('click', closePopupAddCard);
